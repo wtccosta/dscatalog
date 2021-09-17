d3e469230c61de3cacf07c92d79977f5ac97bcdc
Wardes Thiago de Carvalho Costa <wardes.costa@gmail.com>
2021-09-17 09:45:28 -0400

    Test anoter hasAnyRoles cenarios

--- frontweb/src/util/__tests__/auth.spec.ts
+++ frontweb/src/util/__tests__/auth.spec.ts
@@ -20,4 +20,19 @@ describe('hasAnyRoles tests', () => {
     expect(result).toEqual(true);
     ('');
   });
+
+
+    test('should return false when user does note have given role', () => {
+      jest.spyOn(TokenModule, 'getTokenData').mockReturnValue({
+        exp: 0,
+        user_name: 'string',
+        authorities: ['ROLE_OPERATOR'],
+      });
+
+      const result = hasAnyRoles(['ROLE_ADMIN']);
+      expect(result).toEqual(false);
+      ('');
+    });
+
+
 });
